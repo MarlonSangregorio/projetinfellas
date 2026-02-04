774a8f06b16c11928f3ed776450b5d4113690410
MarlonSangregorio <marlonsousa0321@gmail.com>
2026-02-04 03:24:52 -0300

    iframe maps + hidden header scrollY

--- js/script.js
+++ js/script.js
@@ -1,18 +1,39 @@
+//menu
+
 const openBtn = document.querySelector('.menu-toggle');
 const closeBtn = document.querySelector('.menu-close');
 const menu = document.querySelector('.menu-overlay');
 
-openBtn.addEventListener('click', () => {
+function openMenu() {
     menu.classList.add('active');
-});
+}
 
-closeBtn.addEventListener('click', () => {
+function closeMenu() {
     menu.classList.remove('active');
-});
+}
+
+openBtn.addEventListener('click', openMenu);
+closeBtn.addEventListener('click', closeMenu);
 
 // fechar ao clicar em um link
 document.querySelectorAll('.menu-links a').forEach(link => {
-    link.addEventListener('click', () => {
-        menu.classList.remove('active');
-    });
+    link.addEventListener('click', closeMenu);
+});
+
+//scroll da tela
+let lastScroll = 0;
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (menu.classList.contains('active')) return
+
+    const currentScroll = window.scrollY;
+
+    if (currentScroll > lastScroll && currentScroll > 100) {
+        header.classList.add("hide");
+    } else {
+        header.classList.remove("hide");
+    }
+    lastScroll = currentScroll;
 });
